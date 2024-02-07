ef29b440f794c35343788487eddc6e963c59e6ef
Helanka Singhapurage <helankas26@gmail.com>
2024-02-07 12:33:36 +0530

    add env index

---
+++ index.js
@@ -0,0 +1,23 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+
+const port = process.env.SERVER_PORT;
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
+
+try {
+    mongoose.connect('mongodb://localhost:27017/eadp_utility_api_db');
+    app.listen(port, () => {
+        console.log('server up and running!');
+    })
+} catch (e) {
+    console.log(e)
+}
