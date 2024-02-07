eae2f2808e1c69ca06a5bad4c6cd6678ed7bef43
Helanka Singhapurage <helankas26@gmail.com>
2024-02-07 17:12:12 +0530

    add eureka client

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const Eureka = require('eureka-js-client').Eureka;
 require('dotenv').config();
 
 
@@ -9,6 +10,36 @@ const port = process.env.SERVER_PORT;
 
 const app = express();
 app.use(cors());
+
+//=======================================
+const eurekaClient = new Eureka({
+    instance: {
+        app: 'utility-service-api',
+        hostName: 'localhost',
+        instanceId: 'utility-service',
+        ipAddr: '127.0.0.1',
+        port: {
+            '$': port,
+            '@enabled': true
+        },
+        vipAddress: 'jq.test.something.com',
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'MyOwn',
+        },
+    },
+    eureka: {
+        host: '127.0.0.1',
+        port: 8761,
+        servicePath: '/eureka/apps/'
+    }
+});
+
+eurekaClient.start(function (error) {
+    console.log('########################################################');
+    console.log(JSON.stringify(error) || 'Eureka registration complete');
+});
+//=======================================
 
 app.use(bodyParser.urlencoded({extend: false}));
 app.use(bodyParser.json());
@@ -25,3 +56,10 @@ try {
 app.get('/api/v1/test', (req, resp, next) => {
     resp.send(`<h2>Hello User (Utility Service)</h2>`);
 });
+
+process.on('SIGINT', () => {
+    eurekaClient.stop(() => {
+        console.log('server terminated!');
+        process.exit();
+    });
+})
