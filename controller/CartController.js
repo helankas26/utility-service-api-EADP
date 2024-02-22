83029314c036ce7fd4535574304f9f8f71d205c9
Helanka Singhapurage <helankas26@gmail.com>
2024-02-22 16:52:53 +0530

    cart data pagination

--- controller/CartController.js
+++ controller/CartController.js
@@ -2,14 +2,14 @@ const CartSchema = require('../model/cartSchema');
 
 const loadAllData = async (request, response) => {
 
-    const page = parseInt(request.query.page) || 1;
+    const page = parseInt(request.query.page) || 0;
     const size = parseInt(request.query.size) || 10;
 
     try {
         const totalCountData = await CartSchema.countDocuments();
         const totalPages = Math.ceil(totalCountData / size);
 
-        const skip = (page - 1) * size; // offset
+        const skip = page* size; // offset
 
         const data = await CartSchema.find({}).skip(skip).limit(size).exec();
 
